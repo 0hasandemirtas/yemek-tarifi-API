f965ce49eba53bfc4484e086b4681132513c134a
0hasandemirtas <hasandemirtas9@gmail.com>
2024-07-23 00:09:51 +0300

    Rethinkdb başlangıçta tablo oluşturma hataları giderildi.

--- index.js
+++ index.js
@@ -1,125 +1,145 @@
 var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
-var r = require('rethinkdbdash')({
-    servers:[
-        {
-            host: 'localhost',
-            port: 28015
-        }
-]
+var r = require("rethinkdbdash")({
+  servers: [
+    {
+      host: "localhost",
+      port: 28015,
+    },
+  ],
 });
 
-const intialDataBase =async ()=>{
-    const tables = await r.db("test").tableList()
-    if(!tables.includes("recipe")){
-       await r.tableCreate('recipe').run()
-    }
-    
-}
+const intialDataBase = async () => {
+  const tables = await r.db("test").tableList();
+  if (!tables.includes("recipe")) {
+    await r.tableCreate("recipe").run();
+  }
+};
 
 intialDataBase();
 
- 
-
-
-var app=express();
+var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", async(req, res)=>{
-    r.db("test")
-        .table("recipe")
-        .then((recipe)=>{
-            res.statusCode=200;
-            res.send({
-                message:"Success",
-                code: 200,
-                payload:recipe
-            });
-        })
-        .catch((err)=>{
-            res.statusCode=500;
-            res.send({
-                message:err.message,
-                code:500,
-                payload:err
-            });
-        });
+app.get("/", async (req, res) => {
+  r.db("test")
+    .table("recipe")
+    .then((recipe) => {
+      res.statusCode = 200;
+      res.send({
+        message: "Success",
+        code: 200,
+        payload: recipe,
+      });
+    })
+    .catch((err) => {
+      res.statusCode = 500;
+      res.send({
+        message: err.message,
+        code: 500,
+        payload: err,
+      });
+    });
+});
+
+app.get("/:id", async (req, res) => {
+  r.db("test")
+    .table("recipe")
+    .get(req.params.id)
+    .then((recipe) => {
+      res.statusCode = 200;
+      res.send({
+        message: "Success",
+        code: 200,
+        payload: recipe,
+      });
+    })
+    .catch((err) => {
+      res.statusCode = 500;
+      res.send({
+        message: err.message,
+        code: 500,
+        payload: err,
+      });
+    });
 });
 
 app.post("/", async (req, res) => {
-    const newRecipe=req.body;
-    r.db("test")
-        .then("recipe")
-        .insert(newRecipe)
-        .then((recipe)=>{
-            res.statusCode=500;
-            res.send({
-                message:"eklendi",
-                code: 200,
-                payload: recipe
-            });
-        })
-        .catch((err)=>{
-            res.statusCode=500;
-            res.send({
-                message:err.message,
-                code:500,
-                payload:err
-            });
-        });
+  const newRecipe = req.body;
+  console.log(req.body);
+  r.db("test")
+    .table("recipe")
+    .insert(newRecipe)
+    .then((recipe) => {
+      res.statusCode = 201;
+      res.send({
+        message: "eklendi",
+        code: 201,
+        payload: recipe,
+      });
+    })
+    .catch((err) => {
+      res.statusCode = 500;
+      res.send({
+        message: err.message,
+        code: 500,
+        payload: err,
+      });
+    });
 });
 
-app.delete("/", async (req, res) => {
-    const id = req.params.id;
-    r.db("test")
-        .table("recipe")
-        .get(id)
-        .delete()
-        .then((recipe) =>{
-            res.statusCode=200;
-            res.send({
-                message:"silindi",
-                code: 200,
-                payload: recipe
-            });
-        })
-        .catch((err) => {
-            res.statusCode=500;
-            res.send({
-                message:err.message,
-                code:500,
-                payload:err
-            });
-        });
+app.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  r.db("test")
+    .table("recipe")
+    .get(id)
+    .delete()
+    .then((recipe) => {
+      res.statusCode = 200;
+      res.send({
+        message: "silindi",
+        code: 200,
+        payload: recipe,
+      });
+    })
+    .catch((err) => {
+      res.statusCode = 500;
+      res.send({
+        message: err.message,
+        code: 500,
+        payload: err,
+      });
+    });
 });
 
-app.put("/", async(req, res) =>{
-    const id = req.params.id;
-    const updateRecipe = req.body;
-    r.db("test")
-        .table("recipe")
-        .get(id)
-        .update(updateRecipe)
-        .then((recipe) =>{
-            res.statusCode=200;
-            res.send({
-                message:"güncellendi",
-                code: 200,
-                payload: recipe
-            })
-        })
-        .catch((err) => {
-            res.statusCode=500;
-            res.send({
-                message:err.message,
-                code:500,
-                payload:err,
-            });
-        });
+app.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const updateRecipe = req.body;
+  console.log(updateRecipe);
+  r.db("test")
+    .table("recipe")
+    .get(id)
+    .update(updateRecipe)
+    .then((recipe) => {
+      res.statusCode = 200;
+      res.send({
+        message: "güncellendi",
+        code: 200,
+        payload: recipe,
+      });
+    })
+    .catch((err) => {
+      res.statusCode = 500;
+      res.send({
+        message: err.message,
+        code: 500,
+        payload: err,
+      });
+    });
 });
 
-app.listen(3001, ()=>{
-    console.log("3001 portu dinleniyor")
+app.listen(3001, () => {
+  console.log("3001 portu dinleniyor");
 });
